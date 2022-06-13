c18ea0c58bbc53b4f99b5d02fda5dc17b1d1cf4f
Sergey <emelyanchik1980@gmail.com>
2022-06-13 17:04:47 +0300

    Домашнее задание №1 Добавил BottomAppBar+FAB и настроил BottomNavigationDrawerFragment

--- app/src/main/java/ru/geekbrains/material/view/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/ru/geekbrains/material/view/picture/PictureOfTheDayFragment.kt
@@ -3,14 +3,16 @@ package ru.geekbrains.material.view.picture
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.load
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import ru.geekbrains.material.R
 import ru.geekbrains.material.databinding.FragmentPictureOfTheDayBinding
+import ru.geekbrains.material.view.MainActivity
 import ru.geekbrains.material.viewmodel.PictureOfTheDayAppState
 import ru.geekbrains.material.viewmodel.PictureOfTheDayViewModel
 
@@ -32,6 +34,31 @@ class PictureOfTheDayFragment : Fragment() {
         ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.app_bar_fav -> {
+                Log.d("@@@", "app_bar_fav")
+            }
+            R.id.app_bar_settings -> {
+                Log.d("@@@", "app_bar_settings")
+            }
+            android.R.id.home -> {
+                BottomNavigationDrawerFragment.newInstance()
+                    .show(requireActivity().supportFragmentManager, "")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+
+
+    }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -46,6 +73,28 @@ class PictureOfTheDayFragment : Fragment() {
             })
 
         }
+        val bottomSheetBehavior = BottomSheetBehavior.from(binding.lifeHack.bottomSheetContainer)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_DRAGGING -> {}
+                    BottomSheetBehavior.STATE_COLLAPSED -> {}
+                    BottomSheetBehavior.STATE_EXPANDED -> {}
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {}
+                    BottomSheetBehavior.STATE_HIDDEN -> {}
+                    BottomSheetBehavior.STATE_SETTLING -> {}
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                Log.d("@@@", "$slideOffset")
+            }
+
+        })
+        (requireActivity() as MainActivity).setSupportActionBar(binding.bottomAppBar)
+        setHasOptionsMenu(true)
     }
 
 
@@ -55,6 +104,8 @@ class PictureOfTheDayFragment : Fragment() {
             is PictureOfTheDayAppState.Loading -> {}
             is PictureOfTheDayAppState.Success -> {
                 binding.imageView.load(pictureOfTheDayAppState.pictureOfTheDayResponseData.url)
+                binding.lifeHack.title.text =
+                    pictureOfTheDayAppState.pictureOfTheDayResponseData.title
                 // TODO HW скрасить ожидание картинки
             }
         }
