76f7146dba1a35fa4a9960c4a21b7d1b840ce816
Sergey <emelyanchik1980@gmail.com>
2022-06-16 15:38:27 +0300

    Домашнее задание №1 Поправил!

--- app/src/main/java/ru/geekbrains/material/viewmodel/PictureOfTheDayViewModel.kt
+++ app/src/main/java/ru/geekbrains/material/viewmodel/PictureOfTheDayViewModel.kt
@@ -13,45 +13,47 @@ import ru.geekbrains.material.repository.PictureOfTheDayRetrofitImpl
 class PictureOfTheDayViewModel(
     private val liveDataForViewToObserve: MutableLiveData<PictureOfTheDayAppState> = MutableLiveData(),
     private val retrofitImpl: PictureOfTheDayRetrofitImpl = PictureOfTheDayRetrofitImpl()
+) : ViewModel() {
     fun getLiveData(): LiveData<PictureOfTheDayAppState> {
-    return liveDataForViewToObserve
-}
-fun sendServerRequest() {
-    liveDataForViewToObserve.value = PictureOfTheDayAppState.Loading(0)
-    val apiKey: String = BuildConfig.NASA_API_KEY
-    if (apiKey.isBlank()) {
-        liveDataForViewToObserve.value = PictureOfTheDayAppState.Error(Throwable("wrong key"))
-    } else {
-        retrofitImpl.getRetrofit().getPictureOfTheDay(apiKey).enqueue(callback)
+        return liveDataForViewToObserve
     }
-}
 
-fun sendServerRequest(date: String) {
-    liveDataForViewToObserve.value = PictureOfTheDayAppState.Loading(0)
-    val apiKey: String = BuildConfig.NASA_API_KEY
-    if (apiKey.isBlank()) {
-        liveDataForViewToObserve.value = PictureOfTheDayAppState.Error(Throwable("wrong key"))
-    } else {
-        retrofitImpl.getRetrofit().getPictureOfTheDay(apiKey, date).enqueue(callback)
-    }
-}
-
-private val callback = object : Callback<PictureOfTheDayResponseData> {
-    override fun onResponse(
-        call: Call<PictureOfTheDayResponseData>,
-        response: Response<PictureOfTheDayResponseData>
-    ) {
-        if (response.isSuccessful) {
-            response.body()?.let {
-                liveDataForViewToObserve.postValue(PictureOfTheDayAppState.Success(it))
-            }
+    fun sendServerRequest() {
+        liveDataForViewToObserve.value = PictureOfTheDayAppState.Loading(0)
+        val apiKey: String = BuildConfig.NASA_API_KEY
+        if (apiKey.isBlank()) {
+            liveDataForViewToObserve.value = PictureOfTheDayAppState.Error(Throwable("wrong key"))
         } else {
-            // TODO HW
+            retrofitImpl.getRetrofit().getPictureOfTheDay(apiKey).enqueue(callback)
         }
     }
 
-    override fun onFailure(call: Call<PictureOfTheDayResponseData>, t: Throwable) {
-        // TODO HW
+    fun sendServerRequest(date:String) {
+        liveDataForViewToObserve.value = PictureOfTheDayAppState.Loading(0)
+        val apiKey: String = BuildConfig.NASA_API_KEY
+        if (apiKey.isBlank()) {
+            liveDataForViewToObserve.value = PictureOfTheDayAppState.Error(Throwable("wrong key"))
+        } else {
+            retrofitImpl.getRetrofit().getPictureOfTheDay(apiKey,date).enqueue(callback)
+        }
     }
-}
+
+    private val callback = object : Callback<PictureOfTheDayResponseData> {
+        override fun onResponse(
+            call: Call<PictureOfTheDayResponseData>,
+            response: Response<PictureOfTheDayResponseData>
+        ) {
+            if (response.isSuccessful) {
+                response.body()?.let {
+                    liveDataForViewToObserve.postValue(PictureOfTheDayAppState.Success(it))
+                }
+            } else {
+                // TODO HW
+            }
+        }
+
+        override fun onFailure(call: Call<PictureOfTheDayResponseData>, t: Throwable) {
+            // TODO HW
+        }
+    }
 }
