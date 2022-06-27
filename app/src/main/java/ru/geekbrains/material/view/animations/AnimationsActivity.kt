abb64f772c6e176b21dece258561ea6ea362a134
Sergey <emelyanchik1980@gmail.com>
2022-06-27 18:26:47 +0300

    Домашнее задание №5 ConstraintSet анимация.

--- app/src/main/java/ru/geekbrains/material/view/animations/AnimationsActivity.kt
+++ app/src/main/java/ru/geekbrains/material/view/animations/AnimationsActivity.kt
@@ -16,38 +16,35 @@ import androidx.transition.*
 import ru.geekbrains.material.R
 import ru.geekbrains.material.databinding.ActivityAnimationsBinding
 import ru.geekbrains.material.databinding.ActivityAnimationsBonusStartBinding
+import ru.geekbrains.material.databinding.ActivityAnimationsNewBinding
 
 class AnimationsActivity : AppCompatActivity() {
 
     private val duration: Long = 1000
     var isOpen: Boolean = false
-    private lateinit var binding: ActivityAnimationsBonusStartBinding
+    private lateinit var binding: ActivityAnimationsNewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAnimationsBonusStartBinding.inflate(layoutInflater)
+        binding = ActivityAnimationsNewBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.backgroundImage.setOnClickListener {
+        val constraintSetStart = ConstraintSet()
+        val constraintSetEnd = ConstraintSet()
+        constraintSetStart.clone(this, R.layout.include_fab_animations_start)
+        constraintSetEnd.clone(this, R.layout.include_fab_animations_end)
+        binding.anim.fabCenter.setOnClickListener {
 
-            val constraintSet = ConstraintSet()
-            constraintSet.clone(binding.constraintContainer) // через код
-
-            val transition = ChangeBounds()
-            transition.interpolator = AnticipateOvershootInterpolator(5f)
-            transition.duration = 1000
-            TransitionManager.beginDelayedTransition(binding.constraintContainer,transition )
 
             isOpen = !isOpen
-            if(isOpen){
-                constraintSet.connect(R.id.title,ConstraintSet.RIGHT,R.id.backgroundImage,ConstraintSet.RIGHT) // через код
-            // constraintSet.clone(this, R.layout.activity_animations_bonus_end)  // через макет
-            }else{
-                constraintSet.connect(R.id.title,ConstraintSet.RIGHT,R.id.backgroundImage,ConstraintSet.LEFT)     // через код
-            // constraintSet.clone(this, R.layout.activity_animations_bonus_start) // через макет
+            if (isOpen) {
+                constraintSetStart.applyTo(binding.anim.container)
+                //    constraintSet.connect(R.id.title,ConstraintSet.RIGHT,R.id.backgroundImage,ConstraintSet.RIGHT) // через код
+                // constraintSet.clone(this, R.layout.activity_animations_bonus_end)  // через макет
+            } else {
+                constraintSetEnd.applyTo(binding.anim.container)
+                //    constraintSet.connect(R.id.title,ConstraintSet.RIGHT,R.id.backgroundImage,ConstraintSet.LEFT)     // через код
+                // constraintSet.clone(this, R.layout.activity_animations_bonus_start) // через макет
             }
-
-
-            constraintSet.applyTo(binding.constraintContainer)
         }
     }
 }
