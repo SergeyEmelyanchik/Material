fcbafde63a298a9f79b6995a0f19238f28a944ea
Sergey <emelyanchik1980@gmail.com>
2022-06-27 14:48:32 +0300

    Домашнее задание №5 Slide

--- app/src/main/java/ru/geekbrains/material/view/animations/AnimationsActivity.kt
+++ app/src/main/java/ru/geekbrains/material/view/animations/AnimationsActivity.kt
@@ -1,12 +1,10 @@
 package ru.geekbrains.material.view.animations
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.transition.ChangeBounds
-import androidx.transition.Fade
-import androidx.transition.TransitionManager
-import androidx.transition.TransitionSet
+import androidx.transition.*
 import ru.geekbrains.material.databinding.ActivityAnimationsBinding
 
 class AnimationsActivity : AppCompatActivity() {
@@ -20,12 +18,14 @@ class AnimationsActivity : AppCompatActivity() {
 
         binding.button.setOnClickListener {
             val transitionFade = Fade()
+            val transitionSlide = Slide(Gravity.END)
             transitionFade.duration = 3000
             val transitionChangeBounds = ChangeBounds()
             transitionChangeBounds.duration = 5000
             val transitionSet = TransitionSet()
-            transitionSet.addTransition(transitionFade)
+            // transitionSet.addTransition(transitionFade)
             transitionSet.addTransition(transitionChangeBounds)
+            transitionSet.addTransition(transitionSlide)
             TransitionManager.beginDelayedTransition(binding.transitionsContainer, transitionSet)
             isOpen = !isOpen
             binding.text.visibility = if (isOpen) {
