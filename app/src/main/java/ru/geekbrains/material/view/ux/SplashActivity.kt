b9afa576743ed835ffa995878cad18bfda376170
Sergey <emelyanchik1980@gmail.com>
2022-06-29 18:05:30 +0300

    Домашнее задание №8 Добавил продвинутый" способ отображения загрузочного экрана + anim_test.xml.

---
+++ app/src/main/java/ru/geekbrains/material/view/ux/SplashActivity.kt
@@ -0,0 +1,28 @@
+package ru.geekbrains.material.view.ux
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import ru.geekbrains.material.R
+import ru.geekbrains.material.view.MainActivity
+
+@SuppressLint("CustomSplashScreen") // Android 12
+class SplashActivity : AppCompatActivity() { //TODO HW single-activity пытаемся сохранить
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.MyBlueTheme)
+        setContentView(R.layout.activity_splash)
+
+        /*ObjectAnimator.ofFloat(findViewById<ImageView>(R.id.imageView), View.ROTATION, 720f)
+            .setDuration(4000).start()*/
+        findViewById<ImageView>(R.id.imageView).animate().rotationBy(720f).setDuration(4000).start()
+        Handler(mainLooper).postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 3000)
+
+    }
+}
