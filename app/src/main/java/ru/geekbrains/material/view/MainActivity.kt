6c00707f4505f679518c7895edd19a73e37f8c3d
Sergey <emelyanchik1980@gmail.com>
2022-06-29 19:35:52 +0300

    Домашнее задание №8 Добавил продвинутый" способ отображения загрузочного экрана + anim_test.xml.

--- app/src/main/java/ru/geekbrains/material/view/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/material/view/MainActivity.kt
@@ -8,7 +8,7 @@ import ru.geekbrains.material.view.picture.PictureOfTheDayFragment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.MyGreenTheme)
+        setTheme(R.style.MyBlueTheme)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction().replace(
